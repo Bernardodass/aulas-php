3abd2c1c73c6a8223fff84a1e21aaa1db1cd722a
Bernardodass <bernardo113322@gmail.com>
2024-05-14 17:25:39 -0300

    PHP teste começo

---
+++ 01-hello-word.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciando com PHP</title>
+
+
+<!-- CSS -->
+<!-- <style>
+body {color: #4dffff}
+</style> -->
+
+
+
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+   
+
+
+
+
+    <!-- delimitadores de abertura/fechamento PHP -->
+    <?php
+    echo "Olá";
+    echo "<p>Senac Penha é daora</p>";
+    echo "<p style=\"text-align:center\">Senac Penha é daora</p>";
+    ?>
+    <!-- delimitadores de abertura/fechamento PHP -->
+
+
+
+
+</body>
+</html>
