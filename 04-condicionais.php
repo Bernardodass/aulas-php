924a7a00191388d75b3633a4e9bd7e3e488e9766
Bernardodass <bernardo113322@gmail.com>
2024-05-16 14:14:14 -0300

    Update 04-condicionais.php

--- 04-condicionais.php
+++ 04-condicionais.php
@@ -12,12 +12,42 @@
     <hr>
     <h2>Simples (não tem o <code>else</code>)</h2>
 <?php
-
-
 $numero = 50;
 if($numero<100){
     echo "<p>Condição é verdadeira/true</p>";
 }
+?>
+<hr>
+<h2>Composta (usa <code>if/else</code>)</h2>
+<?php
+$numerofrutas = 60;
+if($numerofrutas>30){
+    echo "<p>tem muitas frutas</p>";
+} else {
+    echo "<p>tem umas frutas</p>";
+}
+?>
+<?php
+// controle de estoque
+$produto = "ultrabook";
+$qtdemestoque = 10; // o que temos no momento
+$qtdcritica = 2; //mínimo necessário
+?>
+    <h3>produto: <?=$produto?></h3>
+    <h3>estoque: <?=$qtdemestoque?></h3>
+
+    <?php
+
+if ($qtdemestoque<$qtdcritica) {
+    /* se a quantidade em estoque for abaixo da quantidade crítica, o sistema deve avisar e pedir para repor. */
+    echo "<p>é necessário repor o estoque</p>";
+} else {
+    /* caso contrário, simplesmente falar que o estoque está normal. */
+    echo "<p>o estoque está normal</p>";
+}
+
+
+
 
 
 ?>
