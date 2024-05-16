645929c90c018ff449b810f70b78238a3c66ea56
Bernardodass <bernardo113322@gmail.com>
2024-05-16 15:56:19 -0300

    Refatoração condicionais

--- 05-condicionais-refatorado.php
+++ 05-condicionais-refatorado.php
@@ -8,13 +8,15 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <body>
     <div class="container" >
-    <h1>Condicionais</h1>
+    <h1>Condicionais REFATORADO</h1>
     <hr>
     <h2>Simples (não tem o <code>else</code>)</h2>
 <?php
 $numero = 50;
 if($numero<100){
-    echo "<p>Condição é verdadeira/true</p>";
+    ?>
+    <p>Condição é verdadeira/true</p>
+    <?php
 }
 ?>
 <hr>
@@ -22,6 +24,7 @@ if($numero<100){
 <?php
 $numerofrutas = 60;
 if($numerofrutas>30){
+    
     echo "<p>Tem muitas frutas</p>";
 } else {
     echo "<p>Tem umas frutas</p>";
